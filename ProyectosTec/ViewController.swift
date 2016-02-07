1d72cc1b1a079abf82a077ce1447e9f886735d19
Juan Pablo Hernández <phdez16@gmail.com>
2016-02-06 18:18:20 -0600

    Initial Commit

---
+++ ProyectosTec/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProyectosTec
+//
+//  Created by Pablo Hernández on 6/2/16.
+//  Copyright © 2016 Pablo Hernández. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
